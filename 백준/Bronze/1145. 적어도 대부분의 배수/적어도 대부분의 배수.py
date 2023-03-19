79d5e2bf6be33b734450bdf83595ad64f11aab7a
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-03-19 15:33:07 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 452 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -0,0 +1,12 @@
+nums = list(map(int, input().split()))
+
+i = 0
+while True:
+    i += 1
+    count = 0
+    for num in nums:
+        if i % num == 0:
+            count += 1
+    if count >= 3:
+        print(i)
+        break
