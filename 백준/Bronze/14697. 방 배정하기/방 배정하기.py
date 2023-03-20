576fe6ad1cfc0eb1563dae29a3b4bafb25a98a12
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-03-20 11:33:14 +0900

    [Bronze II] Title: 방 배정하기, Time: 36 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -2,9 +2,9 @@ a, b, c, n = map(int, input().split())
 
 
 def room():
-    for x in range(1, 51):
-        for y in range(x, 51):
-            for z in range(y, 51):
+    for z in range(n//a + 1):
+        for y in range(n//b + 1):
+            for x in range(n//c + 1):
                 if (a * x) + (b * y) + (c * z) == n:
                     return 1
 
