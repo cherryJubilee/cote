7d7f03d2a4687bb26b43c3f4530e7c6e98a5bd25
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-03-31 00:10:49 +0900

    [Silver IV] Title: 최대 GCD, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/9417. 최대 GCD/최대 GCD.py
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.py
@@ -1,8 +1,4 @@
-def gcd(a, b):
-    for i in range(min(a, b), 0, -1):
-        if a % i == 0 and b % i == 0:
-            return i    # 역순으로 반복문이 실행되므로 가장 큰 i 반환
-
+import math
 
 n = int(input())
 for _ in range(n):
@@ -10,6 +6,5 @@ for _ in range(n):
     answer = [1]
     for i in range(0, len(num_list)-1):
         for j in range(i+1, len(num_list)):
-            answer.append(gcd(num_list[i], num_list[j]))
-
+            answer.append(math.gcd(num_list[i], num_list[j]))
     print(max(answer))
