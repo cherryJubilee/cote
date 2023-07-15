a2c24468d76fd8506ec66c4ff890541e294107dd
cherryJubilee <hyewon5026@gmail.com>
2023-07-15 23:02:18 +0900

    같은숫자는싫어1_스택

---
+++ 같은숫자는싫어1.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = []
+    prev = arr[0]
+    for i in range(len(arr)):
+        if arr[i] == prev:
+            continue
+        else:
+            answer.append(prev)
+            prev = arr[i]
+    answer.append(prev)
+    return answer
