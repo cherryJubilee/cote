62763e1594b5d37497a1cf00ae4a87e4ce6d87fe
박혜원 <hyewon@baghyewon-ui-noteubug.local>
2023-07-25 21:43:19 +0900

    합이같은부분집합(DFS)

---
+++ 인프런/합이같은부분집합(DFS).py
@@ -0,0 +1,21 @@
+import sys
+
+def dfs(L, sum):
+    if sum > total // 2:
+        return
+    if L==n:
+        if sum == (total-sum):
+            print("YES")
+            sys.exit(0)
+
+
+    else:
+        dfs(L+1, sum + a[L])
+        dfs(L+1, sum)
+
+
+if __name__ == "__main__":
+    n = int(input())  # 6
+    a = list(map(int, input().split())) # 1 3 5 6 7 10
+    total = sum(a)
+    dfs(0,0)
