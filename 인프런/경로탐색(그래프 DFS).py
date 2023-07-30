c49373be72b3d50e84264de4bd1b34982b3e0204
cherryJubilee <hyewon5026@gmail.com>
2023-07-30 23:24:12 +0900

    unsolved

--- 인프런/경로탐색(그래프 DFS).py
+++ 인프런/경로탐색(그래프 DFS).py
@@ -1,4 +1,4 @@
-
+# 0730
 n, m = map(int, input().split()) # 정점의 수, 간선의 수
 g = [[0] * (n+1) for _ in range(n+1)]
 ch = [0] * (n+1)
@@ -8,4 +8,5 @@ for i in range(m):
 cnt = 0
 ch[1] =1 # 1번노드 방문함. 체크 하고 넘어감
 dfs(1)
+
 
