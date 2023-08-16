923465c505d1faa91b37e5265754ad8e8adda72e
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-08-16 22:01:13 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,20 +1,27 @@
 def solution(s):
+    
     s = s.split(" ") 
     answer = []
-
+    
+    #공백문자가 연속해서 나올 수 있다는거에 주의하자!!!
+    # 참고 # "1qWe    eE ff"를 split(" ") 하면 ['1qWe', '', '', '', 'eE', 'ff']이렇게 된다
+    # 빈문자열도 사용해야하니 for문 돌릴 때 추가해야함!!!
+    
     for i in s:
-        # 문자열 i가 비어있지 않다면
-        if len(i) > 0:
-            # 첫 문자가 알파벳이라면
+        if i != "":
+            # 알파벳 이라면
             if i[0].isalpha():
                 i = i[0].upper() + i[1:].lower()
                 answer.append(i)
-            # 첫 문자가 알파벳이 아니라면
+            # 알파벳이 아니라면 
             else: 
                 i = i[0] + i[1:].lower()
                 answer.append(i)
         else:
             answer.append(i)
-
-    result = " ".join(answer)
+                
+    result =  " ".join(answer)
     return result
+            
+        
+    
