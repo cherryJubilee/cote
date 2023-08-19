ed812a4c35ea50d2bc3104617376b47992be7222
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-08-19 17:25:27 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,11 +1,11 @@
 -- 코드를 입력하세요
-SELECT NAME, DATETIME
-FROM (
-    SELECT i.NAME, i.DATETIME
-    FROM ANIMAL_INS i
-    LEFT JOIN ANIMAL_OUTS o
-    ON i.ANIMAL_ID = o.ANIMAL_ID
-    WHERE o.ANIMAL_ID IS NULL
-    ORDER BY i.DATETIME
-)
+-- 가장 오래 보호소에 있었던 동물 3마리 이름과 보호시작일
+SELECT c.NAME, c.DATETIME
+FROM (SELECT a.NAME, a.DATETIME -- 아직 입양을 못 간 동물 중
+        FROM ANIMAL_INS a
+        LEFT OUTER JOIN ANIMAL_OUTS b 
+          ON a.ANIMAL_ID = b.ANIMAL_ID
+       WHERE b.DATETIME IS NULL
+      ORDER BY a.DATETIME
+    ) c
 WHERE ROWNUM <= 3
