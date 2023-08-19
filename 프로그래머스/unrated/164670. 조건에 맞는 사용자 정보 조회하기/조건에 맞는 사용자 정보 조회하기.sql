1b5e8efcd42d82adae842bf948eef3f40687b078
cherryJubilee <112672507+cherryJubilee@users.noreply.github.com>
2023-08-19 23:35:18 +0900

    [unrated] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/unrated/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,14 +1,12 @@
 -- 코드를 입력하세요
-SELECT USER_ID, 
-        NICKNAME, 
-        CITY || ' ' || STREET_ADDRESS1 || ' ' ||STREET_ADDRESS2, 
-        SUBSTR(TLNO, 1, 3) || '-' || SUBSTR(TLNO, 4, 4) || '-' || SUBSTR(TLNO, 8, 4)
-FROM USED_GOODS_USER -- 사용자 ID, 닉네임, 전체주소, 전화번호를 조회 가능 테이블 
-WHERE USER_ID IN ( SELECT WRITER_ID --  중고 거래 게시물을 3건 이상 등록한 사용자를 비교하기 위한 서브쿼리
-                     FROM USED_GOODS_BOARD
-                  GROUP BY WRITER_ID
-                  HAVING COUNT(WRITER_ID) >= 3
-                  )
-                  
-                  
+SELECT USER_ID,
+    NICKNAME,
+    CITY || ' ' || STREET_ADDRESS1 || ' ' || STREET_ADDRESS2,
+    SUBSTR(TLNO, 1, 3) || '-' || SUBSTR(TLNO, 4, 4) || '-' || SUBSTR(TLNO, 8, 4)
+FROM USED_GOODS_USER
+WHERE USER_ID IN (SELECT WRITER_ID
+                    FROM USED_GOODS_BOARD
+                    GROUP BY WRITER_ID
+                    HAVING COUNT(*) >= 3
+                   )
 ORDER BY USER_ID DESC
