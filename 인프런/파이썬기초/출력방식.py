df30dda27b29bdb2ab032a5c08a65a9416d63bcd
cherryJubilee <hyewon5026@gmail.com>
2023-12-01 14:04:03 +0900

    반복문

--- 인프런/파이썬기초/출력방식.py
+++ 인프런/파이썬기초/출력방식.py
@@ -43,3 +43,50 @@ print(a%b) # 나머지
 print(a**b) # 제곱
 
 
+# 반목문 (for, while)
+a = range(10) 
+print(list(a))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for i in range(1, 11, 2):
+    print(i)
+'''
+1
+3
+5
+7
+9
+'''
+i = 1
+while i < 11:
+    print(i)
+    i += 2
+'''
+1
+3
+5
+7
+9
+'''
+
+# break : 해당 되면 멈춤
+i = 1
+while True:
+    print(i)
+    if i == 5:
+        break
+    i += 1
+'''
+1
+2
+3
+4
+5
+'''
+
+# continue: 해당하면 아래 코드 실행x
+i = 1
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue # 짝수이면 출력 하지 마
+    print(i)
+    
