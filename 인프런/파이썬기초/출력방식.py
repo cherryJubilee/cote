ff2b57375c449517e27784d44371b5ffc13a33b4
cherryJubilee <hyewon5026@gmail.com>
2023-12-01 13:48:18 +0900

    변수입력(map)과 연산자

--- 인프런/파이썬기초/출력방식.py
+++ 인프런/파이썬기초/출력방식.py
@@ -18,3 +18,28 @@ print(a,b,c, sep="\n")
 print(a, end=",") 
 print(b, end=",") 
 print(c) 
+
+# 변수입력과 연산자
+a,b = input("숫자를 입력하세요: ").split()
+c = a+b
+print(type(c)) # <class 'str'>
+print(c)       # 23
+
+# int형으로 바꿔주어야 연산 가능
+a = int(a)
+b = int(b)
+c = a+b
+print(type(c)) # <class 'int'>
+print(c)       # 5
+
+# int형 쓰기 귀찮을 때  map사용하자
+a, b = map(int, input("숫자를 입력하세요: ").split())
+print(a+b)  # 5
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b) # 몫
+print(a%b) # 나머지
+print(a**b) # 제곱
+
+
